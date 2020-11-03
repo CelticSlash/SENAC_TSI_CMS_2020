9cd81de5f482076d0968fdd95445bc68041ad8c7
CelticSlash <37378603+CelticSlash@users.noreply.github.com>
2020-11-03 19:14:22 -0300

    Update meu-primeiro-plugin.php
    Plugin para tags do head

--- plugins/meu-primeiro-plugin/meu-primeiro-plugin.php
+++ plugins/meu-primeiro-plugin/meu-primeiro-plugin.php
@@ -16,5 +16,28 @@
 
      function altera_msg_login()
      {
-         return 'Credenciais Inválidas.';
+        return 'Credenciais Inválidas.';
+     }
+
+     add_action('wp_head', 'colocar_og_tags');
+
+     function colocar_og_tags()
+     {
+         if(is_single())
+         {
+            $dados_do_post = get_post(get_the_ID());
+
+            $nome_do_site = get_bloginfo();
+
+            $titulo = $dados_do_post->post_title
+
+            $resumo = $dados_do_post->post_excerpt;
+
+             echo " \n\n\n
+                    <meta property='og:title' content='" .$titulo. "'>\n
+                    <meta property='og:site_name' content='" .$nome_do_site."'>\n
+                    <meta property='og:url' content='" .get_permalink()."'>\n
+                    <meta property='og:description' content='" .$resumo."'>\n
+                    \n\n\n";
+         }
      }
